adb5d5fd8c0ae90fc5b3c8b7d12f622778e5197c
alize <alizesuchon@gmail.com>
2025-11-24 10:15:49 +0100

    Initial Commit

---
+++ zakfit/zakfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  zakfitApp.swift
+//  zakfit
+//
+//  Created by alize suchon on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct zakfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
